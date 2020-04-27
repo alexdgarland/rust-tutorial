88039a39c333f6bd7edfd3933fd4900a5106cdd5
Alex Garland <alex.d.garland@gmail.com>
2020-04-27 22:20:41 +0100

    5.3 Move calculation of rectangle area into a struct method (and add display format)

--- structs/src/rectangle.rs
+++ structs/src/rectangle.rs
@@ -1,25 +1,35 @@
+use std::fmt;
 
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
-    height: u32
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+impl fmt::Display for Rectangle {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            formatter,
+            "Rectangle - width: {} pixels, height: {} pixels, area: {} square pixels",
+            self.width,
+            self.height,
+            self.area()
+        )
+    }
 }
 
 pub fn demo_rectangle() {
     let rect1 = Rectangle {
         width: 30,
-        height: 50
+        height: 50,
     };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
-
-    // NB this doesn't work if we move rather than borrow when passing struct to the area function
-    println!("The original rectangle is -  {:?}", rect1);
+    println!("The struct is: {}", rect1);
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.height * rectangle.width
-}
