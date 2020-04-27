555b46a5ae4ddde22af3d9af5407b3e6dda30873
Alex Garland <alex.d.garland@gmail.com>
2020-04-27 22:08:25 +0100

    5.2 Refactor rectangle code to use struct

--- structs/src/rectangle.rs
+++ structs/src/rectangle.rs
@@ -1,12 +1,25 @@
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
 pub fn demo_rectangle() {
-    let rect1 = (30, 50);
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50
+    };
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(rect1)
+        area(&rect1)
     );
+
+    // NB this doesn't work if we move rather than borrow when passing struct to the area function
+    println!("The original rectangle is -  {:?}", rect1);
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.height * rectangle.width
 }
