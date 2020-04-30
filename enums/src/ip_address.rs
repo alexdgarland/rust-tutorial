2d3e4652466a20a87629a9c76ae5979effa899d4
Alex Garland <alex.d.garland@gmail.com>
2020-04-30 23:31:58 +0100

    6.1 Switch to enum fields rather than struct, including use of built in IpvXAddr types

--- enums/src/ip_address.rs
+++ enums/src/ip_address.rs
@@ -1,53 +1,45 @@
 use std::fmt;
 use std::fmt::Formatter;
+use std::net::{Ipv4Addr, Ipv6Addr};
 
-
-#[derive(Debug)]
-enum IpAddressVersion {
-    V4,
-    V6,
-}
-
-impl fmt::Display for IpAddressVersion {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
-
-struct IpAddress {
-    version: IpAddressVersion,
-    address: String,
+enum IpAddress {
+    V4(Ipv4Addr),
+    V6(Ipv6Addr)
 }
 
 impl IpAddress {
-    fn version_name(&self) -> String {
-        format!("IP {}", self.version.to_string().to_ascii_lowercase())
+
+    fn info(&self) -> (&str, String) {
+        match self {
+            IpAddress::V4(address) => ("v4", address.to_string()),
+            IpAddress::V6(address) => ("v6", address.to_string())
+        }
+    }
+
+    fn ip_version(&self) -> &str {
+        self.info().0
+    }
+
+    fn address(&self) -> String {
+        self.info().1
     }
 }
 
-fn route(ip_address: &IpAddress) {
-    println!(
-        "Routing to {} via {} {}!",
-        ip_address.address,
-        match ip_address.version {
-            IpAddressVersion::V6 => "new and improved",
-            _ => "perfectly acceptable"
-        },
-        ip_address.version_name()
-    )
+impl fmt::Display for IpAddress {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{} via IP {}", self.address(), self.ip_version())
+    }
 }
 
 pub fn demo_ip_address() {
-    let home = IpAddress {
-        version: IpAddressVersion::V4,
-        address: String::from("127.0.0.1"),
-    };
+    let home = IpAddress::V4(
+        Ipv4Addr::new(127, 0, 0, 1)
+    );
 
-    let loopback = IpAddress {
-        version: IpAddressVersion::V6,
-        address: String::from("::1"),
-    };
+    let loopback = IpAddress::V6(
+        Ipv6Addr::new(0, 0, 0 , 0 , 0 , 0, 0 , 1)
+    );
 
-    route(&home);
-    route(&loopback);
+    println!("{}", home);
+    println!("{}", loopback);
 }
