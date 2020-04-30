10d04fad42a8eac9a0ca9ac21d9afd20550774d0
Alex Garland <alex.d.garland@gmail.com>
2020-04-30 22:28:51 +0100

    6.1 Tweak dummy route method including implementing enum tostring

--- enums/src/ip_address.rs
+++ enums/src/ip_address.rs
@@ -1,3 +1,6 @@
+use std::fmt;
+use std::fmt::Formatter;
+
 
 #[derive(Debug)]
 enum IpAddressVersion {
@@ -5,13 +8,19 @@ enum IpAddressVersion {
     V6
 }
 
+impl fmt::Display for IpAddressVersion {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
 fn route(ip_version: &IpAddressVersion) {
-    println!(
-        "Routing via {}!",
-        match ip_version {
-            IpAddressVersion::V4 => "IP v4",
-            IpAddressVersion::V6 => "new and improved IP v6"
-        }
+    println!("Routing via {} IP {}!",
+             match ip_version {
+                 IpAddressVersion::V6 => "new and improved",
+                 _ => "perfectly acceptable"
+             },
+             ip_version.to_string().to_ascii_lowercase()
     )
 }
 
