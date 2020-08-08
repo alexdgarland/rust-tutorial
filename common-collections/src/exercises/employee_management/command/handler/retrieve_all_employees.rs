54e51975e605f8d145843fa98eebfaa686421322
Alex Garland <alex.d.garland@gmail.com>
2020-08-08 01:29:57 +0100

    WIP - add new-format handler to list all employees

---
+++ common-collections/src/exercises/employee_management/command/handler/retrieve_all_employees.rs
@@ -0,0 +1,87 @@
+use super::{ParsedArgMap, CommandHandler, CommandExecutor};
+use crate::exercises::employee_management::employee_store::EmployeeStore;
+use regex::Regex;
+
+const MATCH_PATTERN_DESCRIPTION: &'static str = "Retrieve all departments";
+const REGEX_PATTERN: &'static str = r"^Retrieve all departments$";
+
+pub fn get_retrieve_all_employees_handler<E: EmployeeStore>() -> CommandHandler<E> {
+    let executor: CommandExecutor<E> = |_arg_map: ParsedArgMap, store: &mut E| {
+        info!("Retrieving full employee list");
+        for dept_info in store.retrieve_all_employees() {
+            info!("{} - {}", dept_info.department, dept_info.employee_names.join(", "));
+        }
+        Ok(())
+    };
+
+    CommandHandler::new(
+        MATCH_PATTERN_DESCRIPTION,
+        Regex::new(REGEX_PATTERN).unwrap(),
+        vec![],
+        executor,
+    )
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::get_retrieve_all_employees_handler;
+    use crate::exercises::employee_management::command::handler::{HandleCommand, CommandHandler};
+    use mockall::predicate::eq;
+    use crate::exercises::employee_management::employee_store::{MockEmployeeStore, DepartmentInfo};
+    use log::Level;
+
+    const MATCHING_COMMAND: &str = "Retrieve all departments";
+    const NON_MATCHING_COMMAND: &'static str = "Get me all the departments!";
+
+    fn run_test_against_matcher(command_text: &str, expected_return: bool) {
+        let test_handler: CommandHandler<MockEmployeeStore> = get_retrieve_all_employees_handler();
+        assert_eq!(test_handler.matches_command_text(command_text), expected_return)
+    }
+
+    #[test]
+    fn test_matcher_handles_matching_pattern() {
+        run_test_against_matcher(MATCHING_COMMAND, true);
+    }
+
+    #[test]
+    fn test_matcher_handles_non_matching_pattern() {
+        run_test_against_matcher(NON_MATCHING_COMMAND, false);
+    }
+
+    #[test]
+    fn test_executor_calls_expected_method_on_store() {
+        testing_logger::setup();
+
+        let mock_return_department_infos = vec![
+            DepartmentInfo{
+                department: "Pie Analysis".to_string(),
+                employee_names: vec!["Bob Bobertson".to_string(), "Weebl Bull".to_string()]
+            },
+            DepartmentInfo{
+                department: "Stealthy Buccaneering".to_string(),
+                employee_names: vec!["Chris the Ninja Pirate".to_string()]
+            }
+        ];
+        let mut mock_store = MockEmployeeStore::new();
+        mock_store
+            .expect_retrieve_all_employees()
+            .times(1)
+            .with().return_const(mock_return_department_infos);
+
+        let result = get_retrieve_all_employees_handler()
+            .execute_command(MATCHING_COMMAND, &mut mock_store);
+
+        assert_eq!(result, Ok(()));
+
+        testing_logger::validate(|captured_logs| {
+            assert_eq!(captured_logs.len(), 3);
+            assert_eq!(captured_logs[0].body, "Retrieving full employee list");
+            assert_eq!(captured_logs[0].level, Level::Info);
+            assert_eq!(captured_logs[1].body, "Pie Analysis - Bob Bobertson, Weebl Bull");
+            assert_eq!(captured_logs[1].level, Level::Info);
+            assert_eq!(captured_logs[2].body, "Stealthy Buccaneering - Chris the Ninja Pirate");
+            assert_eq!(captured_logs[2].level, Level::Info);
+        });
+    }
+}
