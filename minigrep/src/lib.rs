055e651fe7327d1dbbd8acce6166236b2332e876
Alex Garland <alex.d.garland@gmail.com>
2020-08-16 21:20:17 +0100

    12.4 Add tests and implement search function TDD-style

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::fs;
 
+#[derive(Eq, PartialEq, Debug)]
 pub struct Config<'a> {
     pub query: &'a str,
     pub filename: &'a str,
@@ -18,10 +19,52 @@ impl<'a> Config<'a> {
     }
 }
 
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    // A more declarative version than provided in the tutorial
+    contents
+        .lines()
+        .filter(|l| l.contains(query))
+        .collect()
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Config, search};
+
+    #[test]
+    fn test_config_should_parse_correctly_from_valid_args() {
+        let args = vec!["minigrep".to_string(), "some pattern".to_string(), "filename.type".to_string()];
+        let expected = Config {
+            query: "some pattern",
+            filename: "filename.type"
+        };
+        assert_eq!(Config::new(&args), Ok(expected));
+    }
+
+    #[test]
+    fn test_config_parsing_should_error_on_not_enough_args() {
+        let args = vec!["minigrep".to_string(), "some pattern".to_string()];
+        assert_eq!(Config::new(&args), Err("not enough arguments"));
+    }
+
+
+    #[test]
+    fn test_search_one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -15,7 +15,6 @@ fn main() {
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
-
         process::exit(1);
     }
 }
